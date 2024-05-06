802f536c60803c3af3d2e94ea9bf858a256ba300
irma12345678 <yaya98383@gmail.com>
2024-05-06 15:04:43 +0700

    first commit

---
+++ user/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../galeri/img/J.png" type="icon">
+    <title>Beranda | SMK 1 TRIPLE 'J'</title>
+</head>
+<body>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand d-flex align-align-item" href="#">
+                <img src="../galeri/img/j.png" height="50" width="50" alt="logo"
+                    class="d-inline-block align-text-top me-3">
+                <div class="profile">
+                    <h4 class="my-0">SMK DIGITAL INDONESIA</h4>
+                    <P class="my-0">Maju seiring perkembangan zaman</P>
+                </div>
+            </a>
+        </div>
+    </nav>
+    <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="../galeri/uploads/tj1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="../galeri/uploads/coffe.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="../galeri/uploads/tjs.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <!-- Columns are always 50% wide, on mobile and desktop -->
+      <div class="card mb-3" style="max-width: 100%;">
+  <div class="row no-gutters">
+    <div class="col-md-6">
+      <img src="../galeri/uploads/tjs.jpg" class="card-img" alt="Gambar">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title">SMK 1 TRIPLE J</h5>
+        <p class="card-text"> Pendidikan yang diperoleh di sekolah membentuk dasar untuk karir dan profesi di masa depan. Siswa yang memiliki pendidikan yang kuat memiliki lebih banyak kesempatan untuk sukses dalam kehidupan dan berkontribusi pada pembangunan masyarakat.</p>
+        <p class="card-text"><small class="text-muted"></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="card mb-3" style="max-width: 100%;">
+  <div class="row no-gutters">
+    <div class="col-md-6">
+    <div class="card-body">
+        <h5 class="card-title">Informasi Terkini</h5>
+        <p class="card-text"> Serah Terima SISWA PKL di Institut Agama Islam Tazkia Sentul
+PKL dicarikan oleh Sekolah
+Sekolah pilihan terbaik</p>
+        <p class="card-text"><small class="text-muted"></small></p>
+      </div>
+    </div>
+
+
+    <div class="col-md-6">
+    <img src="../galeri/uploads/tj2.jpg" class="card-img" alt="Gambar">
+      
+    </div>
+
+
+  </div>
+</div>
+<!-- footer -->
+<div class="container-custom">
+  <div class="footer">
+    <div class="col-md-6"></div>
+    <div class="maps">
+            <iframe src="https://maps.google.com/maps?q=smks 1 triple j&amp;t=&amp;z=10&amp;ie=UTF8&amp;iwloc=&amp;output=embed" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+            <div class="alamat">
+              <center>
+                <h4 class="mb-3">SMK 1 Triple "J"</h4>
+                <h6>Jl. Landbow No.01, Karang Asem Barat, Kec. Citeureup, Kabupaten Bogor, Jawa Barat 16810</h6>
+                </center>
+                <center>
+                <div class="wrapper-contact">
+                    <div class="item"><i class="fa fa-phone"></i>(021) 8757384 </div>
+                    <div class="item"><i class="fa fa-envelope"></i> <a href="mailto:smktj1@gmail.com">smktj1@gmail.com</a> </div>
+                    <div class="item"><i class="fab fa-facebook-square"></i> <a href="">SMK 1 Triple &quot;J&quot; Citereup</a> </div>
+                    <div class="item"><i class="fab fa-instagram"></i> <a href="">@smk_1_triple_j</a> </div>
+                    
+                </div>  </center>
+            </div>
+        </div>
+        <div class="text-center mt-3"><h6>Copyright 2024 SMK 1 Triple J</h6></div>
+    </div>
+ 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
